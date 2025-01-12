cf5ae327ae4a57c91bd12bc38ce2090f9eeabfc3
Kadyrofficial <kadyr.gullyyew@gmail.com>
2025-01-12 16:53:39 +0100

    commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 
