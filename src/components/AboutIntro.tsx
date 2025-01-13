778b4fe03ac894ff4eb9a999b2845dc7396dbcf6
Kadyrofficial <kadyr.gullyyew@gmail.com>
2025-01-13 01:32:04 +0100

    hve

--- src/components/AboutIntro.tsx
+++ src/components/AboutIntro.tsx
@@ -1,17 +1,7 @@
-import Header from '../components/Header';
-import LanguageSelector from '../components/LanguageSelector';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
-import Services from '../components/Services';
-
-function About() {
+function AboutIntro() {
   
   return (
     <>
-    <Navigation />
-      <Header />
-      <LanguageSelector />
-      <section className='w-full p-[20px] bg-lightgray flex flex-col items-center'>
         <div className='bg-white py-[20px] w-full 2xl:px-[150px] flex flex-col md:flex-row-reverse md:justify-center md:items-center'>
           <div className='md:w-1/2 max-w-[900px]'>
             <img src="../public/deliverer.svg" alt="" />
@@ -21,16 +11,9 @@ function About() {
             <p className='font-bold text-[20px] lg:text-[30px] text-center'>HOJALYK JEMGYÝETI</p>
             <p className='font-[400] text-[18px] lg:text-[25px] xl:text-[30px] text-center p-[30px] max-w-[700px] '>“Mahal” H.J 2021-nji ýyldan bäri Hytaý, Malaziýa, Türkiýe, Wýetnam, Taýland döwletlerinden gereke bolan harytlary we enjamlary gözlemek, tapmak, satyn almak we getirip bermek hyzmatyny edýär.</p>
           </div>
-          
-        </div> <br />
-        <div className='bg-white'>
-          <Services />
         </div>
-      </section>
-      
-      <Footer />
     </>
   )
 }
 
-export default About
+export default AboutIntro
